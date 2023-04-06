18be3f9fd71daa5504b980e9d6694d3ebf6337ca
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2023-04-06 17:33:46 -0300

    Capture keyboard events

--- src/main.py
+++ src/main.py
@@ -8,15 +8,29 @@ def main():
     screen = pygame.display.set_mode(size)
 
     pygame.display.set_caption("Project Game")
-    done = False
+    running = True
     clock = pygame.time.Clock()
 
-    while not done:
+    while running:
         clock.tick(60)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                done = True
+                running = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT or event.key == ord("a"):
+                    print("left")
+                if event.key == pygame.K_RIGHT or event.key == ord("d"):
+                    print("right")
+                if event.key == pygame.K_UP or event.key == ord("w"):
+                    print("jump")
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == ord("a"):
+                    print("left stop")
+                if event.key == pygame.K_RIGHT or event.key == ord("d"):
+                    print("right stop")
 
         pygame.draw.circle(screen, "blue", [60, 250], 40)
 
