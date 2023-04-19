3a79420c9ba2fb1198354e35bb95c07aff2734fd
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2023-04-19 15:13:57 -0300

    Add start menu

--- src/game.py
+++ src/game.py
@@ -1,13 +1,18 @@
-import os
-
 import pygame
 
 import settings
+from interface import Menu
 from level import Level
 from player import Player
 
 
 class Game:
+    NEW_GAME = pygame.event.custom_type()
+    EXIT = pygame.QUIT
+
+    def __init__(self) -> None:
+        pass
+
     def setup(self):
         pygame.init()
         pygame.joystick.init()
@@ -15,24 +20,45 @@ class Game:
         size = [settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT]
         self.screen = pygame.display.set_mode(size)
         self.clock = pygame.time.Clock()
-        self.level = Level()
-        self.internal_events = Player.EVENTS
+        self.internal_events = []
 
     def run(self):
         self.setup()
+        start_menu = {
+            "new game": self.NEW_GAME,
+            "load game": self.NEW_GAME,
+            "settings": self.NEW_GAME,
+            "exit": self.EXIT,
+        }
+        self.menu = Menu(start_menu)
+
+        self.main_loop = self.load_menu
+
         running = True
-
         while running:
-            dt = self.clock.tick(100) / 1000
-
             for event in pygame.event.get(exclude=self.internal_events):
                 if event.type == pygame.QUIT:
                     running = False
+
                 if event.type == Level.FINISHED:
                     print("Game Over")
                     running = False
 
-            self.level.run(dt)
+                if event.type == self.NEW_GAME:
+                    self.level = Level()
+                    self.internal_events = Player.EVENTS
+                    self.main_loop = self.new_game
+
+            dt = self.clock.tick(100) / 1000
+
+            self.main_loop(dt)
+
             pygame.display.update()
 
         pygame.quit()
+
+    def load_menu(self, dt):
+        self.menu.run()
+
+    def new_game(self, dt):
+        self.level.run(dt)
--- src/interface.py
+++ src/interface.py
@@ -1,10 +1,114 @@
 from abc import ABC, abstractmethod
-from typing import Iterable
+from typing import Any, Iterable
 
 import pygame
 
 from player import BasePlayer
 from world import World
+
+
+class Menu:
+    class Item(pygame.sprite.Sprite):
+        def __init__(self, text, event_id, *groups: pygame.sprite.Group) -> None:
+            super().__init__(*groups)
+            self.text = text
+            self.event = pygame.event.Event(event_id)
+
+            font = pygame.sysfont.SysFont("freesansbold", 100)
+            font_padding = 20
+            text_surf = font.render(text, True, "blue")
+            x, y = text_surf.get_size()
+            padded_text_surf = pygame.surface.Surface(
+                (x + 2 * font_padding, y + 2 * font_padding)
+            )
+            padded_text_surf.blit(text_surf, (font_padding, font_padding))
+
+            self.original_image = padded_text_surf
+            self.highlighted_image = self.original_image.copy()
+            self.image = self.original_image.copy()
+            pygame.draw.rect(
+                self.highlighted_image, "blue", self.highlighted_image.get_rect(), 1
+            )
+
+            self.rect = self.image.get_rect()
+            self.enabled = True
+            self.highlighted = False
+
+        def update(self, *args: Any, **kwargs: Any) -> None:
+            self.image = (
+                self.highlighted_image if self.highlighted else self.original_image
+            )
+            return super().update(*args, **kwargs)
+
+    def __init__(self, items: dict[str, int]) -> None:
+        self.font = pygame.sysfont.SysFont("freesansbold", 100)
+        self._items = [self.Item(txt, id) for txt, id in items.items()]
+        self.all_items = pygame.sprite.Group()
+        self.all_items.add(self._items)
+
+        self.display = pygame.display.get_surface()
+        self.surface = pygame.surface.Surface(self.display.get_size())
+        self.highlighted_item = 0
+        self.draw_static()
+        pygame.joystick.init()
+        joysticks = [
+            pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())
+        ]
+        self.joystick = joysticks[0] if joysticks else None
+
+    def draw_static(self):
+        padding = 80
+        font_padding = 20
+
+        w, h = self.display.get_size()
+        w, h = w - 2 * padding, h - 2 * padding
+        menu_rect = pygame.rect.Rect(padding, padding, w, h)
+
+        pygame.draw.rect(self.surface, "blue", menu_rect, 1)
+
+        for i, item in enumerate(self.all_items):
+            if i == self.highlighted_item:
+                item.highlighted = True
+            item.rect.x = padding + font_padding
+            item.rect.y = (
+                padding + font_padding + i * (item.image.get_size()[1] + font_padding)
+            )
+
+    def run(self):
+        self.all_items.draw(self.surface)
+
+        d_pad_events = pygame.event.get(pygame.JOYHATMOTION)
+        d_pad_events = [e for e in d_pad_events if e.hat == 0]
+        if d_pad_events:
+            d = d_pad_events[0].value[1]
+            if d == 1:
+                self.highlight_prev()
+            elif d == -1:
+                self.highlight_next()
+        button_events = pygame.event.get(pygame.JOYBUTTONDOWN)
+        if button_events:
+            b = button_events[0].button
+            if b == 0:
+                self.select()
+
+        self.all_items.update()
+        self.display.blit(self.surface, (0, 0))
+
+    def highlight_prev(self):
+        self.highlight_item(self.highlighted_item - 1)
+
+    def highlight_next(self):
+        self.highlight_item(self.highlighted_item + 1)
+
+    def highlight_item(self, index: int):
+        index = int(pygame.math.clamp(index, 0, len(self._items) - 1))
+        self._items[self.highlighted_item].highlighted = False
+        self.highlighted_item = index
+        self._items[self.highlighted_item].highlighted = True
+
+    def select(self):
+        event = self._items[self.highlighted_item].event
+        pygame.event.post(event)
 
 
 class BaseInterfaceElement(ABC):
