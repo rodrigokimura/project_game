138133054225c8a08b5a807ff7baae9ded0a0588
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2023-04-27 13:50:13 -0300

    Fix tests

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -1,7 +1,9 @@
+from unittest.mock import MagicMock, patch
+
 import pygame
 import pytest
 
-from interface import Camera
+from camera import Camera
 from player import BasePlayer
 from world import World
 
@@ -20,45 +22,66 @@ class TestCamera:
     def world(self):
         return World((100, 100), 10, 10)
 
-    def test_camera_on_center(self, player: BasePlayer, world: World):
+    @pytest.fixture
+    def mocked_setup(self):
+        with patch.object(Camera, "_setup") as m:
+            m.return_value = None
+            yield m
+
+    def test_camera_on_center(
+        self, mocked_setup: MagicMock, player: BasePlayer, world: World
+    ):
         player.rect.center = (50, 50)
         camera = Camera((20, 20), player, world)
 
         rect = camera.get_rect()
 
+        mocked_setup.assert_called()
         assert rect.center == (50, 50)
 
-    def test_camera_on_topleft_corner(self, player: BasePlayer, world: World):
+    def test_camera_on_topleft_corner(
+        self, mocked_setup: MagicMock, player: BasePlayer, world: World
+    ):
         player.rect.center = (5, 5)
         camera = Camera((20, 20), player, world)
 
         rect = camera.get_rect()
 
+        mocked_setup.assert_called()
         assert rect.topleft == (0, 0)
 
-    def test_camera_on_topright_corner(self, player: BasePlayer, world: World):
+    def test_camera_on_topright_corner(
+        self, mocked_setup: MagicMock, player: BasePlayer, world: World
+    ):
         player.rect.center = (90, 5)
         camera = Camera((20, 20), player, world)
 
         rect = camera.get_rect()
 
+        mocked_setup.assert_called()
         assert rect.top == 0
         assert rect.right == 100
 
-    def test_camera_on_bottomleft_corner(self, player: BasePlayer, world: World):
+    def test_camera_on_bottomleft_corner(
+        self, mocked_setup: MagicMock, player: BasePlayer, world: World
+    ):
         player.rect.center = (5, 90)
         camera = Camera((20, 20), player, world)
 
         rect = camera.get_rect()
 
+        mocked_setup.assert_called()
         assert rect.bottom == 100
         assert rect.left == 0
 
-    def test_camera_on_bottomright_corner(self, player: BasePlayer, world: World):
+    def test_camera_on_bottomright_corner(
+        self, mocked_setup: MagicMock, player: BasePlayer, world: World
+    ):
         player.rect.center = (90, 90)
         camera = Camera((20, 20), player, world)
 
         rect = camera.get_rect()
 
+        mocked_setup.assert_called()
         assert rect.bottom == 100
         assert rect.right == 100
